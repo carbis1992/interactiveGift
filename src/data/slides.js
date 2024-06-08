b45c5ffad60efff88a2a8458e9a01327edb5956d
carla <car.bis@hotmail.com>
2024-06-08 13:38:25 +0200

    upd images url

--- src/data/slides.js
+++ src/data/slides.js
@@ -1,11 +1,11 @@
 export const slidesData = [
   {
-    src: 'src/assets/image1.jpg',
+    src: './src/assets/image1.jpg',
   },
   {
-    src: 'src/assets/image2.jpg',
+    src: './src/assets/image2.jpg',
   },
   {
-    src: 'src/assets/image3.jpg',
+    src: './src/assets/image3.jpg',
   },
   ];
