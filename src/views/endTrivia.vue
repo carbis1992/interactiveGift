9c226fddbb25cbb4c7bb04b3a227f0bf3f2b7852
carla <car.bis@hotmail.com>
2024-06-13 13:21:21 +0200

    fix video end trivia

--- src/views/endTrivia.vue
+++ src/views/endTrivia.vue
@@ -1,9 +1,10 @@
-<!-- src/views/EndQuiz.vue -->
 <template>
-  <video autoplay muted loop id="myVideo">
+  <div class="video-wrapper">
+    <video autoplay muted loop id="myVideo">
       <source :src="video" type="video/mp4">
     </video>
-  <div class="d-flex flex-column justify-center align-center mx-auto" style="width: 90%; margin-top: 55%;">
+  </div>
+  <div class="content-wrapper d-flex flex-column justify-center align-center">
     <v-card variant="outlined" class="card-content">
       <v-card-title class="text-center" style="font-weight: 700;">GANASTE !!!!</v-card-title>
       <v-card-text style="font-size: large;">Este año decidí regalarte un voucher de 200 € para que lo canjees este verano en la escuela de buceo de tu preferencia. 
@@ -36,14 +37,36 @@ confetti({
 </script>
 
 <style lang="scss" scoped>
-video{
-  width: -webkit-fill-available;
-  height: 100vh;
-  object-fit: cover;
+.video-wrapper {
   position: fixed;
   top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  z-index: -1;
 }
-.card-content{
+
+video {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.content-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  margin-top: 55%;
+  margin-left: 5%;
+  position: relative;
+  z-index: 1;
+  text-align: center;
+}
+
+.card-content {
   background-color: rgb(95 158 160 / 69%);
   color: #fff;
   border-color: #26A69A;
